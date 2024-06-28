f341913acf7ac2b2058026a75b0efd9a83b87ee0
tranhuuphucuong <cuong@koodaa.com>
2024-06-28 18:56:43 +0700

    header scroll

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+try {
+  if (typeof jQuery === "undefined") {
+    console.log("JQuery is not defined");
+  }
+
+  const $ = jQuery;
+
+  $(document).ready(function () {
+    console.log("Document is ready");
+
+    // detect srolling using jquery
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $("header").addClass("on-scroll");
+      } else {
+        $("header").removeClass("on-scroll");
+      }
+    });
+  });
+} catch (e) {
+  console.log(e);
+}
