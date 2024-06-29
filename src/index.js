4be8955f0b5893e939021fc68fb12c0e23b17fc4
tranhuuphucuong <cuong@koodaa.com>
2024-06-29 11:22:40 +0700

    fly in animation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,65 @@
+// @ts-check
+// define type for jQuery
+/**
+ * @typedef {import('jquery')} jQuery
+ */
+var isDebuging = true;
+var log = isDebuging ? console.log : function () {};
 try {
   if (typeof jQuery === "undefined") {
-    console.log("JQuery is not defined");
+    log("JQuery is not defined");
   }
 
   const $ = jQuery;
 
-  $(document).ready(function () {
-    console.log("Document is ready");
+  $(function () {
+    log("Document is ready");
+    var $window = $(window);
 
-    // detect srolling using jquery
-    $(window).scroll(function () {
+    // header animation
+    $window.on("scroll", function () {
       if ($(this).scrollTop() > 100) {
         $(".main-header-bar").addClass("on-scroll");
       } else {
         $(".main-header-bar").removeClass("on-scroll");
       }
     });
+
+    // in view animation
+    function checkForInView() {
+      var $slide = $('[class*="animate-"]');
+      var window_height = $window.height() || 900;
+      var window_top_position = $window.scrollTop() || 0;
+      var window_bottom_position = window_top_position + window_height;
+
+      var i = 0;
+      var time = -100;
+      $.each($slide, function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight() || 0;
+        var element_top_position = ($element.offset() || {}).top || 0;
+        var element_center_position = element_top_position + element_height / 2;
+
+        if ($window.width() < 1024) {
+          element_center_position = element_top_position + element_height / 4;
+        }
+
+        if (element_top_position <= window_top_position) {
+          $element.addClass("inView");
+        } else if (element_center_position <= window_bottom_position) {
+          if (!$element.hasClass("inView")) {
+            setTimeout(function () {
+              $element.addClass("inView");
+            }, (time += 200));
+          }
+        } else {
+          $element.removeClass("inView");
+        }
+      });
+    }
+    $window.on("resize scroll", checkForInView);
+    $window.trigger("scroll");
   });
 } catch (e) {
-  console.log(e);
+  log(e);
 }
