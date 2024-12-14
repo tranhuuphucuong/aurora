c602eca8a2ce6e18b665585b49ace13d539a77cb
tranhuuphucuong <tranhuu.phucuong@gmail.com>
2024-12-14 17:09:22 +0700

    header style

--- src/biomark/index.js
+++ src/biomark/index.js
@@ -16,6 +16,23 @@ try {
   $(function () {
     log("Document is ready");
     var $window = $(window);
+
+    $window.on("scroll", function () {
+      const scrollTop = $window.scrollTop() || 0;
+      // header animation
+      if ((scrollTop || 0) > 70) {
+        $(".main-header-bar").addClass("on-scroll");
+      } else {
+        $(".main-header-bar").removeClass("on-scroll");
+      }
+
+      // check to add or remove class onScroll
+      if (scrollTop > 340) {
+        $("#menu-container").addClass("onScroll");
+      } else {
+        $("#menu-container").removeClass("onScroll");
+      }
+    });
   });
 } catch (e) {
   log(e);
