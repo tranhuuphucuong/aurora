57ef559c96f19dd04d99ea1295e9b160b506d519
tranhuuphucuong <tranhuu.phucuong@gmail.com>
2024-12-17 14:25:48 +0700

    fly in animation

--- src/biomark/index.js
+++ src/biomark/index.js
@@ -49,6 +49,41 @@ try {
         }
       }
     });
+
+    // in view animation
+    function checkForInView() {
+      var $slide = $('[class*="animate-"]');
+      var window_height = $window.height() || 900;
+      var window_top_position = $window.scrollTop() || 0;
+      var window_bottom_position = window_top_position + window_height;
+
+      var i = 0;
+      var time = -100;
+      $.each($slide, function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight() || 0;
+        var element_top_position = ($element.offset() || {}).top || 0;
+        var element_center_position = element_top_position + element_height / 2;
+
+        if (($window.width() || 0) < 1024) {
+          element_center_position = element_top_position + element_height / 4;
+        }
+
+        if (element_top_position <= window_top_position) {
+          $element.addClass("inView");
+        } else if (element_center_position <= window_bottom_position) {
+          if (!$element.hasClass("inView")) {
+            setTimeout(function () {
+              $element.addClass("inView");
+            }, (time += 200));
+          }
+        } else {
+          $element.removeClass("inView");
+        }
+      });
+    }
+    $window.on("resize scroll", checkForInView);
+    $window.trigger("scroll");
   });
 } catch (e) {
   log(e);
